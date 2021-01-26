481cad7227595cc9834a904218bdbc6686cbfd05
jongin <bill1224@naver.com>
2021-01-27 02:17:29 +0900

    01/27 add login/register in laravel

---
+++ yourima/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    // 아래의 username을 추가하여 이메일 로그인에서 아이디 로그인으로 변경.
+    public function username()
+    {
+        return 'user_id'; // 필드의 네임을 여러분의 선택.
+    }
+}
---
+++ yourima/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('이름');
+            $table->string('user_id')->unique()->comment('아이디'); // 요기 추가
+            $table->unsignedSmallInteger('user_level')->comment('권한'); // 요기 추가
+            $table->string('email')->unique()->comment('이메일');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password')->comment('비밀번호');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
+}
