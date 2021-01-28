d7ac140f35d7c982e36e4588b7afb4ddca790ce1
jongin <bill1224@naver.com>
2021-01-29 02:22:35 +0900

    01/29 tailwindcss

--- test/resources/views/layout.blade.php
+++ test/resources/views/layout.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title', 'laravel')</title>
+  <link rel="stylesheet" href="{{ mix('css/tailwind.css') }}">
 </head>
 <body>
   <ul>
@@ -11,7 +12,7 @@
     <li><a href="/ruby_on_rails">ruby on rails</a></li>
     <li><a href="/django">django</a></li>
   </ul>
-
+  <div class="bg-red-800">hello</div>
   @yield('content')
 </body>
 </html>
