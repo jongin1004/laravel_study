427641a0fa4a4872cabfa79e40340774937c80a4
jongin <bill1224@naver.com>
2021-01-31 14:13:46 +0900

    01/31 if auth

--- test/app/Http/Controllers/TaskController.php
+++ test/app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Task;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
@@ -12,7 +13,9 @@ class TaskController extends Controller
     {   
         // $Tasks = DB::table('tasks')->get();
         // $Tasks = Task::latest() ->where('user_id', auth()->id())-> get();
-        $Tasks = auth() -> user() -> tasks()->latest()->get(); #로그인한 유저가 가진 tasks를 알려줘  최신순서로 그리고 그 값을 get으로 가져와 
+        // $Tasks = auth() -> user() -> tasks()->latest()->get(); #로그인한 유저가 가진 tasks를 알려줘  최신순서로 그리고 그 값을 get으로 가져와 
+        $Tasks = Task::latest()->get();
+
 
         return view('tasks.index', [
             'Tasks' => $Tasks
@@ -61,10 +64,12 @@ class TaskController extends Controller
         // abort_if(auth() -> id() != $task->user_id, 403);
 
         // abort_if(!auth()->user()->owns($task), 403);  //로그인한 유저의 유저정보를 보여준다. 그 유저가 task를 소유하고 있지않으면 403에러를 보여줘라 
-        abort_unless(auth()->user()->owns($task), 403);
+        // abort_unless(auth()->user()->owns($task), 403);
+        $user = User::find($task-> user_id);        
 
         return view('tasks.show', [
-            'task' => $task
+            'task' => $task,
+            'user' => $user
         ]);
     }
 
@@ -90,7 +95,9 @@ class TaskController extends Controller
     }
 
     public function destroy(Task $task)
-    {
+    {   
+        abort_unless(auth()->user()->owns($task), 403);
+        
         $task -> delete();
 
         return redirect('/tasks');
--- test/resources/views/tasks/show.blade.php
+++ test/resources/views/tasks/show.blade.php
@@ -1,19 +1,28 @@
 @extends('layout')
 
 @section('content')
-<div class="px-64 mt-5">
-    <div class="float-right">
-        <a href="/tasks/{{ $task -> id }}/edit">
-            <button class="bg-yellow-400 px-4 text-white hover:bg-yellow-600">edit</button>
+<div class="px-64 mt-4">
+    <div class="flex">
+        <a href="/tasks" class="flex-1">
+            <h1 class="font-bold text-3xl">detail Task</h1>
         </a>
-        <form action="/tasks/{{ $task -> id }}" method="POST">
-            @method('delete')
-            @csrf
-            <button class="flex-initial bg-red-500 px-2 hover:bg-red-300">Delete</button>
-        </form>
+        @if (auth()->id() == $task -> user_id)
+            <a href="/tasks/{{ $task -> id }}/edit">
+                <button class="flex-initial bg-green-500 px-4 py-2 mx-2 hover:bg-green-300">Edit</button>
+            </a>
+        
+            <form action="/tasks/{{ $task -> id }}" method="POST">
+                @method('delete')
+                @csrf
+                <button class="flex-initial bg-red-500 px-4 py-2 hover:bg-red-300">Delete</button>
+            </form>
+        @endif
     </div>
-    <h1 class="text-3xl font-bold">Task</h1>
-    <div>Title : {{ $task -> title}} <small class="float-right">created_at {{ $task -> created_at }}</small></div>
-    body : {{ $task -> body}}
+
+    title : {{ $task -> title }} <small class="float-right">created_at {{ $task -> created_at }}</small><br>
+    <small class="float-right">updated_at {{ $task -> updated_at }}</small><br>
+    <small class="float-right">작성자 {{ $user->name }}</small><br>
+    body
+    <div class="border p-3 my-3">{{ $task -> body }}</div>    
 </div>
 @endsection
