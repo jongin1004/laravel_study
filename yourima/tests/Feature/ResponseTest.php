9e29e53d4399fec0df126920145bd24ace849d9f
jongin <bill1224@naver.com>
2021-02-09 11:10:54 +0900

    02/09 ResponseTest

---
+++ yourima/tests/Feature/ResponseTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ResponseTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        // $this->assertTrue(true);
+
+        // $response = $this->json('POST', '/user', ['name'=>'sally']);
+
+        // $response
+        // ->assertStatus(201)
+        // ->assertJson([
+        //     'created'=>true
+        // ]);
+    }
+}
