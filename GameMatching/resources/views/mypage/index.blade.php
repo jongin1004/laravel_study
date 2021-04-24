7280bec862f319d5bb16e95bee50a907865987eb
jongin <bill1224@naver.com>
2021-04-25 00:19:38 +0900

    유저정보 global css 적용해야함

--- GameMatching/resources/views/mypage/index.blade.php
+++ GameMatching/resources/views/mypage/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
     <div class="flex h-full">
         <div class="w-1/5 border-r-2 border-solid border-gray-600">
             <a href="/mypage/request">
@@ -21,9 +22,48 @@
         </div>
         <div class="w-4/5 flex flex-col">
             @if (substr(url()->previous(), -6, ) == "mypage")
-                <div class="">
-                    {{ $user->id }}                    
-                </div>   
+                <table class="table">                
+                    <tbody>
+                        <tr>
+                            <th scope="col" colspan="2" class="text-center">
+                                기본정보
+                            </th>
+                        </tr>
+                        <tr>
+                            <th scope="row" class="w-48 text-center">
+                                <div>아이디</div>
+                            </th>
+                            <td>
+                                {{ $user->name }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">
+                                <div>메일주소</div>
+                            </th>
+                            <td>
+                                {{ $user->email }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">
+                                <div>가입일</div>
+                            </th>
+                            <td>
+                                {{ $user->created_at }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">
+                                <div>경험치 / 레벨</div>
+                            </th>
+                            <td>
+                                경험치 : {{ $user->experience_point }} / 
+                                레벨 : {{ floor($user->experience_point/10) }}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table> 
             @endif
                        
             @yield('request_friend')
