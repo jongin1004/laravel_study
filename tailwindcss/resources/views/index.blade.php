d1315a79f70c58582344c2cf53cef284f00e89fa
jongin <bill1224@naver.com>
2021-04-26 23:41:45 +0900

    index 책리스트 보여주는 CSS

--- tailwindcss/resources/views/index.blade.php
+++ tailwindcss/resources/views/index.blade.php
@@ -1,5 +1,97 @@
 @extends('layouts.main')
 
 @section('content')
-    <div>Index of movies</div>
+    <div class="container mx-auto px-4 pt-16">
+        <div class="popular-movies">
+            <h2 class="uppercase tracking-wider text-pink-500 text-lg font-semibold">POPULAR MOVIES</h2>
+            {{-- grid로 행분할을 하는데 lg:풀 화면일때에는 5개에서 부터 제일 작은 경우에는 열의 개수가 1개로 반응형으로 만듦 --}}
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-16">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/a.jpg" alt="a" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300 mt-1">신뢰성공학</a>
+                        <div class="flex items-center text-gray-400 text-sm">
+                            <svg class="w-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M23.363 8.584l-7.378-1.127L12.678.413c-.247-.526-1.11-.526-1.357 0L8.015 7.457.637 8.584a.75.75 0 00-.423 1.265l5.36 5.494-1.267 7.767a.75.75 0 001.103.777L12 20.245l6.59 3.643a.75.75 0 001.103-.777l-1.267-7.767 5.36-5.494a.75.75 0 00-.423-1.266z" fill="#ffc107"/></svg>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20, 2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/a.jpg" alt="a" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300 mt-1">신뢰성공학</a>
+                        <div class="flex items-center text-gray-400 text-sm">
+                            <svg class="w-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M23.363 8.584l-7.378-1.127L12.678.413c-.247-.526-1.11-.526-1.357 0L8.015 7.457.637 8.584a.75.75 0 00-.423 1.265l5.36 5.494-1.267 7.767a.75.75 0 001.103.777L12 20.245l6.59 3.643a.75.75 0 001.103-.777l-1.267-7.767 5.36-5.494a.75.75 0 00-.423-1.266z" fill="#ffc107"/></svg>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20, 2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/a.jpg" alt="a" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300 mt-1">신뢰성공학</a>
+                        <div class="flex items-center text-gray-400 text-sm">
+                            <svg class="w-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M23.363 8.584l-7.378-1.127L12.678.413c-.247-.526-1.11-.526-1.357 0L8.015 7.457.637 8.584a.75.75 0 00-.423 1.265l5.36 5.494-1.267 7.767a.75.75 0 001.103.777L12 20.245l6.59 3.643a.75.75 0 001.103-.777l-1.267-7.767 5.36-5.494a.75.75 0 00-.423-1.266z" fill="#ffc107"/></svg>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20, 2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/a.jpg" alt="a" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300 mt-1">신뢰성공학</a>
+                        <div class="flex items-center text-gray-400 text-sm">
+                            <svg class="w-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M23.363 8.584l-7.378-1.127L12.678.413c-.247-.526-1.11-.526-1.357 0L8.015 7.457.637 8.584a.75.75 0 00-.423 1.265l5.36 5.494-1.267 7.767a.75.75 0 001.103.777L12 20.245l6.59 3.643a.75.75 0 001.103-.777l-1.267-7.767 5.36-5.494a.75.75 0 00-.423-1.266z" fill="#ffc107"/></svg>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20, 2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/a.jpg" alt="a" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray:300 mt-1">신뢰성공학</a>
+                        <div class="flex items-center text-gray-400 text-sm">
+                            <svg class="w-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M23.363 8.584l-7.378-1.127L12.678.413c-.247-.526-1.11-.526-1.357 0L8.015 7.457.637 8.584a.75.75 0 00-.423 1.265l5.36 5.494-1.267 7.767a.75.75 0 001.103.777L12 20.245l6.59 3.643a.75.75 0 001.103-.777l-1.267-7.767 5.36-5.494a.75.75 0 00-.423-1.266z" fill="#ffc107"/></svg>
+                            <span class="ml-1">85%</span>
+                            <span class="mx-2">|</span>
+                            <span>Feb 20, 2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>       
+            </div>
+        </div>
+    </div>
 @endsection
